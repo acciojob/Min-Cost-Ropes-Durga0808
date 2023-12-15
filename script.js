d96e5bf9f04f59b4271f3357225a23ed8d03a8bd
Durga Prasad Bejawada <117895509+Durga0808@users.noreply.github.com>
2023-12-15 17:27:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,22 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+  if(arr.length===0)return 0;
+
+	arr.sort((a,b)=>a-b);
+	let cost=0;
+	while(arr.length>1){
+		const first=arr.shift();
+		const second=arr.shift();
+
+		const sum=first+second;
+		cost+=sum;
+
+		arr.push(sum);
+
+		arr.sort((a,b)=>a-b);
+	}
+	return cost;
 }
 
 module.exports=mincost;
